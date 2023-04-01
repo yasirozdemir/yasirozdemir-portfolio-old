9a29aec416eccb15fff0396ee74f8c816e4f1d72
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-04-01 15:34:00 +0300

    nav

--- src/components/AnimetedRoutes/index.js
+++ src/components/AnimetedRoutes/index.js
@@ -2,11 +2,13 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import About from "../About";
 import LandingPage from "../LandingPage";
 import { AnimatePresence } from "framer-motion";
+import CustomNav from "../CustomNav";
 
 const AnimatedRoutes = () => {
   const loc = useLocation();
   return (
     <AnimatePresence mode="wait">
+      <CustomNav loc={loc} />
       <Routes location={loc} key={loc.pathname}>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<About />} />
