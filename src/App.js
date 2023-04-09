d5dd5f0d29509aa1ff540b8df820b89f023322a3
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-04-09 16:27:53 +0300

    contact update & alerts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import { BrowserRouter } from "react-router-dom";
 import AnimatedRoutes from "./components/AnimetedRoutes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AnimatedRoutes />
-    </BrowserRouter>
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <BrowserRouter>
+        <AnimatedRoutes />
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,78 +2,108 @@ import { Col, Container, Row } from "react-bootstrap";
 import "./style.css";
 import emailjs from "@emailjs/browser";
 import { useRef } from "react";
+import { toast } from "react-toastify";
 
 const Contact = ({ observer }) => {
   const contactElements = document.querySelectorAll(".hidden");
   contactElements.forEach((el) => observer.observe(el));
+
+  const alertOptions = {
+    position: "top-center",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  };
 
   const { REACT_APP_SERV_ID, REACT_APP_TEMP_ID, REACT_APP_USER_KEY } =
     process.env;
   const form = useRef();
   const sendEmail = async (e) => {
     e.preventDefault();
-    const result = await emailjs.sendForm(
-      REACT_APP_SERV_ID,
-      REACT_APP_TEMP_ID,
-      form.current,
-      REACT_APP_USER_KEY
-    );
-    console.log(result);
+    try {
+      const result = await emailjs.sendForm(
+        REACT_APP_SERV_ID,
+        REACT_APP_TEMP_ID,
+        form.current,
+        REACT_APP_USER_KEY
+      );
+      console.log(result);
+      if (result.status === 200) {
+        toast.success("Your message has been sent!", alertOptions);
+        document.querySelector("#contact form").reset();
+      } else toast.error("Something went wrong :(", alertOptions);
+    } catch (error) {
+      toast.error(error, alertOptions);
+    }
   };
 
   return (
-    <Container>
-      <Row id="contact" className="justify-content-center pr-lg-3 mb-4 mb-lg-5">
-        <Col
-          xs={12}
-          sm={10}
-          md={6}
-          className="contact__left_up d-flex flex-column justify-content-center align-items-center pb-3 mb-4 pb-lg-0 mb-lg-0 hidden px-2 px-sm-0"
+    <>
+      <Container>
+        <Row
+          id="contact"
+          className="justify-content-center pr-lg-3 mb-4 mb-lg-5"
         >
-          <h1 className="text-center mb-0">Contact Me!</h1>
-          <div className="img-wrapper">
-            <img
-              src="https://res.cloudinary.com/yasirdev/image/upload/v1680865177/portfolio/Contact-us_imkk4f.png"
-              alt="Illustration from freesvgillustration.com"
-              style={{ maxWidth: "100%", maxHeight: "400px" }}
-            />
-          </div>
-        </Col>
-        <Col
-          xs={12}
-          sm={10}
-          md={6}
-          className="contact__right_down px-2 px-sm-0"
-        >
-          <form ref={form} onSubmit={sendEmail}>
-            <div className="group hidden">
-              <input type="text" name="user_name" placeholder="Name" required />
-            </div>
-            <div className="group hidden">
-              <input
-                type="email"
-                name="user_email"
-                placeholder="Email"
-                required
+          <Col
+            xs={12}
+            sm={10}
+            md={6}
+            className="contact__left_up d-flex flex-column justify-content-center align-items-center pb-3 mb-4 pb-lg-0 mb-lg-0 hidden px-2 px-sm-0"
+          >
+            <h1 className="text-center mb-0">Contact Me!</h1>
+            <div className="img-wrapper">
+              <img
+                src="https://res.cloudinary.com/yasirdev/image/upload/v1680865177/portfolio/Contact-us_imkk4f.png"
+                alt="Illustration from freesvgillustration.com"
+                style={{ maxWidth: "100%", maxHeight: "400px" }}
               />
             </div>
-            <div className="group hidden">
-              <textarea
-                rows="8"
-                name="message"
-                placeholder="Your message"
-                required
-              />
-            </div>
-            <div className="group hidden">
-              <button type="submit" className="mx-0">
-                Send
-              </button>
-            </div>
-          </form>
-        </Col>
-      </Row>
-    </Container>
+          </Col>
+          <Col
+            xs={12}
+            sm={10}
+            md={6}
+            className="contact__right_down px-2 px-sm-0"
+          >
+            <form ref={form} onSubmit={sendEmail}>
+              <div className="group hidden">
+                <input
+                  type="text"
+                  name="user_name"
+                  placeholder="Name"
+                  required
+                />
+              </div>
+              <div className="group hidden">
+                <input
+                  type="email"
+                  name="user_email"
+                  placeholder="Email"
+                  required
+                />
+              </div>
+              <div className="group hidden">
+                <textarea
+                  rows="8"
+                  name="message"
+                  placeholder="Your message"
+                  required
+                />
+              </div>
+              <div className="group hidden">
+                <button type="submit" className="mx-0">
+                  Send
+                </button>
+              </div>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
