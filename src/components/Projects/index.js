ad9b3ea1e14dddf61671ac2567b840e3c4e378e2
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-04-10 16:43:02 +0300

    projects card :green_book:

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,3 +1,4 @@
+import ProjectCard from "./ProjectCard";
 import "./style.css";
 import { Col, Container, Row } from "react-bootstrap";
 
@@ -31,75 +32,27 @@ const Projects = ({ observer }) => {
         sm={2}
         md={3}
       >
-        <Col className="hidden">
-          <div className="project-card">
-            <div>
-              <img
-                src="https://media.licdn.com/dms/image/D4D22AQG9J2dYwO6b8A/feedshare-shrink_2048_1536/0/1680953358932?e=1683763200&v=beta&t=h8P3MhVFp3x8znRxxAIlc17GgF3BvSc4VOPe49COD1c"
-                alt="project"
-                className="w-100"
-              />
-            </div>
-            <div>
-              <h5 className="text-center">LinkedIn Clone</h5>
-              <p className="m-0">
-                A clone that users can create their profiles, add experiences,
-                and see what others posted.
-              </p>
-            </div>
-            <div className="badge-container">
-              <span className="tool-badge react">React</span>
-              <span className="tool-badge redux">Redux</span>
-              <span className="tool-badge bootstrap">Bootstrap</span>
-            </div>
-          </div>
-        </Col>
-        <Col className="hidden">
-          <div className="project-card">
-            <div>
-              <img
-                src="https://media.licdn.com/dms/image/D4D22AQGaCX1YyyS93A/feedshare-shrink_2048_1536/0/1680159831974?e=1683763200&v=beta&t=0r5HcjNEng5ksRnk-ExHeKggOeEl8tea8YjnFrdpGGc"
-                alt="project"
-                className="w-100"
-              />
-            </div>
-            <div>
-              <h5 className="text-center">Spotify Clone</h5>
-              <p className="m-0">
-                A clone that users can play music, have a look at the albums and
-                artist.
-              </p>
-            </div>
-            <div className="badge-container">
-              <span className="tool-badge html">HTML</span>
-              <span className="tool-badge css">CSS</span>
-              <span className="tool-badge javascript">JavaScript</span>
-            </div>
-          </div>
-        </Col>
-        <Col className="hidden">
-          <div className="project-card">
-            <div>
-              <img
-                src="https://media.licdn.com/dms/image/C4D22AQGlDhx_vNY-7A/feedshare-shrink_2048_1536/0/1671395931002?e=1683763200&v=beta&t=n35obbg6yQNPq99Cw-eT6k3jRAHLuStcZXakbIlJTZE"
-                alt="project"
-                className="w-100"
-              />
-            </div>
-            <div>
-              <h5 className="text-center">Epicode Benchmark Clone</h5>
-              <p className="m-0">
-                Epicode's benchmark webpage. It's basically a quiz app with 10
-                questions, a timer, and a result page.
-              </p>
-            </div>
-            <div className="badge-container">
-              <span className="tool-badge html">HTML</span>
-              <span className="tool-badge css">CSS</span>
-              <span className="tool-badge javascript">JavaScript</span>
-            </div>
-          </div>
-        </Col>
+        <ProjectCard
+          ghLink="https://github.com/pappkartoni/U2-BW-Deezify"
+          imgSrc="https://media.licdn.com/dms/image/D4D22AQG9J2dYwO6b8A/feedshare-shrink_2048_1536/0/1680953358932?e=1683763200&v=beta&t=h8P3MhVFp3x8znRxxAIlc17GgF3BvSc4VOPe49COD1c"
+          title="LinkedIn Clone"
+          description="A clone that users can create their profiles, add experiences, and see what others posted."
+          toolsArray={["react", "redux", "bootstrap", "css"]}
+        />
+        <ProjectCard
+          ghLink="https://github.com/pappkartoni/U2-BW-Deezify"
+          imgSrc="https://media.licdn.com/dms/image/D4D22AQGaCX1YyyS93A/feedshare-shrink_2048_1536/0/1680159831974?e=1683763200&v=beta&t=0r5HcjNEng5ksRnk-ExHeKggOeEl8tea8YjnFrdpGGc"
+          title="Spotify Clone"
+          description="A clone that users can play music, have a look at the albums and artist."
+          toolsArray={["html", "css", "javascript", "bootstrap"]}
+        />
+        <ProjectCard
+          ghLink="https://github.com/Flovedev/U1-W4-BuildWeek"
+          imgSrc="https://media.licdn.com/dms/image/C4D22AQGlDhx_vNY-7A/feedshare-shrink_2048_1536/0/1671395931002?e=1683763200&v=beta&t=n35obbg6yQNPq99Cw-eT6k3jRAHLuStcZXakbIlJTZE"
+          title="Epicode Benchmark Clone"
+          description="Epicode's benchmark webpage. It's basically a quiz app with 10 questions, a timer, and a result page."
+          toolsArray={["html", "css", "javascript"]}
+        />
       </Row>
     </Container>
   );
